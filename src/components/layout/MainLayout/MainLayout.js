7294a918a4b770fc904b227d870ce28c583f0013
Adam Becker <amkbecker@gmail.com>
2021-12-27 19:11:17 +0100

    Add MUI

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+//import Box from '@mui/material/Box';
 import PageNav from '../PageNav/PageNav';
+import Container from '@mui/material/Container';
 
 const MainLayout = (props) => (
-  <div>
-    <PageNav />
-    {props.children}
+  <div className='MainLayout'>
+    <AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          <PageNav />
+        </Toolbar>
+      </Container>  
+    </AppBar>
+    <Container maxWidth='lg'>
+      <Toolbar/>
+      {props.children}
+    </Container>
   </div>
 );
 
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from './PageNav.module.scss';
 import { NavLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const PageNav = () => (
-  <nav>
-    <NavLink exact to={`process.env.PUBLIC_URL/`} activeClassName={'active'}>Dashboard</NavLink>
-    <NavLink to={`process.env.PUBLIC_URL/login`} activeClassName={'active'}>Login</NavLink>
-    <NavLink to={`process.env.PUBLIC_URL/bookings`} activeClassName={'active'}>Bookings</NavLink>
-    <NavLink to={`process.env.PUBLIC_URL/ordering`} activeClassName={'active'}>Ordering</NavLink>
-    <NavLink to={`process.env.PUBLIC_URL/kitchen`} activeClassName={'active'}>Kitchen</NavLink>    
+  <nav className={styles.component}>
+    <Button className={styles.link} component={NavLink} exact to={`process.env.PUBLIC_URL/`} activeClassName={'active'}>Dashboard</Button>
+    <Button className={styles.link} component={NavLink} to={`process.env.PUBLIC_URL/login`} activeClassName={'active'}>Login</Button>
+    <Button className={styles.link} component={NavLink} to={`process.env.PUBLIC_URL/bookings`} activeClassName={'active'}>Bookings</Button>
+    <Button className={styles.link} component={NavLink} to={`process.env.PUBLIC_URL/ordering`} activeClassName={'active'}>Ordering</Button>
+    <Button className={styles.link} component={NavLink} to={`process.env.PUBLIC_URL/kitchen`} activeClassName={'active'}>Kitchen</Button>    
   </nav>
 );
 
