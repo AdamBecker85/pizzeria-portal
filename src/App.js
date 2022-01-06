bc0a95be6198317d01949dff28fc27b44d053e8e
Adam Becker <amkbecker@gmail.com>
2022-01-06 13:04:04 +0100

    Add redux

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import BookingsBookingId from './components/views/BookingsBookingId/BookingsBook
 import BookingsBookingNew from './components/views/BookingsBookingNew/BookingsBookingNew';
 import BookingsEventId from './components/views/BookingsEventId/BookingsEventId';
 import BookingsEventNew from './components/views/BookingsEventNew/BookingsEventNew';
-import Ordering from './components/views/Ordering/Ordering';
+import OrderingContainer from './components/views/Ordering/OrderingContainer';
 import OrderNew from './components/views/OrderNew/OrderNew';
 import OrderId from './components/views/OrderId/OrderId';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import { StylesProvider } from '@mui/styles';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const theme = createTheme({
   palette: {
@@ -30,27 +33,29 @@ const theme = createTheme({
 
 function App() {
   return (
-    <BrowserRouter basename={`/panel`}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/bookings`} component={Bookings}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/bookings/booking/:id`} component={BookingsBookingId}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/bookings/booking/new`} component={BookingsBookingNew}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/bookings/event/:id`} component={BookingsEventId}></Route>         
-              <Route exact path={`${process.env.PUBLIC_URL}/bookings/event/new`} component={BookingsEventNew}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/new`} component={OrderNew}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={OrderId}></Route>
-              <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}></Route>
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StylesProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={`/panel`}>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/bookings`} component={Bookings}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/bookings/booking/:id`} component={BookingsBookingId}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/bookings/booking/new`} component={BookingsBookingNew}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/bookings/event/:id`} component={BookingsEventId}></Route>         
+                <Route exact path={`${process.env.PUBLIC_URL}/bookings/event/new`} component={BookingsEventNew}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={OrderingContainer}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/new`} component={OrderNew}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={OrderId}></Route>
+                <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}></Route>
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -1,99 +1,124 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Ordering.module.scss';
-//import {Link} from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-
-
-const demoContent = [
-  {id: '1', status: 'free', order: null},
-  {id: '2', status: 'thinking', order: null},
-  {id: '3', status: 'ordered', order: 123},
-  {id: '4', status: 'prepared', order: 234},
-  {id: '5', status: 'delivered', order: 345},
-  {id: '6', status: 'paid', order: 456},
-];
-
-const renderActions = status => {
-  switch (status) {
-    case 'free':
-      return (
-        <>
-          <Button>thinking</Button>
-          <Button>new order</Button>
-        </>
-      );
-    case 'thinking':
-      return (
-        <Button>new order</Button>
-      );
-    case 'ordered':
-      return (
-        <Button>prepared</Button>
-      );
-    case 'prepared':
-      return (
-        <Button>delivered</Button>
-      );
-    case 'delivered':
-      return (
-        <Button>paid</Button>
-      );
-    case 'paid':
-      return (
-        <Button>free</Button>
-      );
-    default:
-      return null;
+class Ordering extends React.Component {
+  static propTypes = {
+    fetchTables: PropTypes.func,
+    loading: PropTypes.shape({
+      active: PropTypes.bool,
+      error: PropTypes.oneOfType([PropTypes.bool,PropTypes.string]),
+    }),
+    tables: PropTypes.array,
   }
-};
 
-const Ordering = () => (
-  <div className={styles.component}>
-    <Paper >
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Table</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Order</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {demoContent.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell>
-                {row.status}
-              </TableCell>
-              <TableCell>
-                {row.order && (
-                  <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                    {row.order}
-                  </Button>
-                )}
-              </TableCell>
-              <TableCell>
-                {renderActions(row.status)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+  componentDidMount(){
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
 
-    <Button className={styles.button} variant="contained" href={`/bookings/order/:id`} activeClassName={'active'}>Booking Id</Button>
-    <Button className={styles.button} variant="contained" href={`/bookings/order/new`} activeClassName={'active'}>Booking New</Button>
-  </div>
-);
+  renderActions(status){
+    switch (status) {
+      case 'free':
+        return (
+          <>
+            <Button>thinking</Button>
+            <Button>new order</Button>
+          </>
+        );
+      case 'thinking':
+        return (
+          <Button>new order</Button>
+        );
+      case 'ordered':
+        return (
+          <Button>prepared</Button>
+        );
+      case 'prepared':
+        return (
+          <Button>delivered</Button>
+        );
+      case 'delivered':
+        return (
+          <Button>paid</Button>
+        );
+      case 'paid':
+        return (
+          <Button>free</Button>
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { loading: { active, error }, tables } = this.props;
+
+    if(active || !tables.length){
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if(error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <div className={styles.component}>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Table</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Order</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tables.map(row => (
+                  <TableRow key={row.id}>
+                    <TableCell component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell>
+                      {row.status}
+                    </TableCell>
+                    <TableCell>
+                      {row.order && (
+                        <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                          {row.order}
+                        </Button>
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {this.renderActions(row.status)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+          <Button className={styles.button} variant="contained" href={`/bookings/order/:id`} activeClassName={'active'}>Booking Id</Button>
+          <Button className={styles.button} variant="contained" href={`/bookings/order/new`} activeClassName={'active'}>Booking New</Button>
+        </div>
+      );
+    }
+  }
+}
 
 export default Ordering;
+
+
